d3e91459c4564c61fbe0b3773021519e653fa56b
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-09-01 14:27:31 -0600

    fix: delete old files

--- src/utils/ManageRepositoryError.ts
+++
@@ -1,8 +0,0 @@
-import { CoreError, Errors } from "utils/Errors";
-
-export function manageRepositoryError(error: any): CoreError {
-  if (error instanceof CoreError) {
-    throw new CoreError(error.code as Errors, error.message);
-  }
-  throw new CoreError("SERVER_ERROR");
-}
